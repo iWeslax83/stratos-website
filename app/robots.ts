b0a082c7e9c083321ceb31620a30c1de1a3f83c1
iWeslax83 <emirsakarya00@gmail.com>
2026-05-31 20:20:48 +0300

    chore(admin): disallow /admin in robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/"],
+      disallow: ["/api/", "/admin", "/api/admin"],
     },
     sitemap: "https://stratosiha.vercel.app/sitemap.xml",
   };
